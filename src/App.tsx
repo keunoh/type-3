285de1910adf9b4b87f6e66b95e23ab282d8330a
keunoh <rmsdhcjswo1@gmail.com>
2025-01-14 00:20:02 +0900

    HelloWorld

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import axios from 'axios';
+import AxiosMockAdapter from "axios-mock-adapter";
+import { faker } from "@faker-js/faker";
+
+import "antd/dist/antd";
+import { Button } from "antd";
+import { useState } from 'react';
+
+const mock = new AxiosMockAdapter(axios);
+
+const posts = [...Array(3)].map((_, index) => {
+  const setIndex = index + 1;
+  return {
+    id: `postId-${setIndex}`,
+    title: faker.lorem.words(),
+    content: faker.lorem.lines(2),
+    image: `${faker.animal.bear}`
+  }
+});
+
+mock.onGet("/posts").reply(() => {
+  try {
+    const results = posts;
+    return [200, results];
+  } catch (error) {
+    console.error(error);
+    return [500, { meesage: "Internal sever error" }];
+  }
+});
+
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+
+  const onClick = async (e: any) => {
+    const mockData = await axios.get("/posts");
+    console.log(mockData);
+
+    setPosts(mockData.data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Button onClick={onClick} style={{ margin: "2rem" }}>
+        버튼
+      </Button>
+
+    </>
   );
 }
 
