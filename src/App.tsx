0272865d5f53b4c09bccea3d5ce810ca3f8e677b
keunoh <rmsdhcjswo1@gmail.com>
2025-01-15 00:47:57 +0900

    Feat: State && Functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
 
+import { useState } from 'react';
 import './Form.css';
+import { resolve } from 'path';
 
-export default function App({
-  status = 'empty'
-}) {
+export default function App() {
+
+  const [answer, setAnswer] = useState<string>('');
+  const [error, setError] = useState<any | null>(null);
+  const [status, setStatus] = useState<string>('typing');
+
   if (status === 'success') {
     return <h1>That's right!</h1>
+  }
+
+  async function handleSubmit(e: any) {
+    e.preventDefault();
+    setStatus('submitting');
+    try {
+      await submitForm(answer);
+      setStatus('success');
+    } catch (err: any) {
+      setStatus('typing');
+      setError(err);
+    }
+  }
+
+  function handleTextareaChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    console.log("handleTextareaChange");
+    console.log(e);
+    setAnswer(e.target.value);
+  }
+
+  function submitForm(answer: string) {
+    return new Promise<void>((resolve, reject) => {
+      setTimeout(() => {
+        let shouldError = answer.toLowerCase() !== 'lima'
+        if (shouldError) {
+          reject(new Error('Good guess buy a wrong answer. Try again!'));
+        } else {
+          resolve();
+        }
+      }, 1500);
+    });
   }
 
   return (
@@ -14,21 +50,23 @@ export default function App({
       <p>
         In which city is there a billboard that turns air into drinkable water?
       </p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <textarea
+          value={answer}
+          onChange={handleTextareaChange}
           disabled={
             status === 'submitting'
           }
         />
         <br />
         <button disabled={
-          status === 'empty' || status === 'submitting'
+          answer.length === 0 || status === 'submitting'
         }>
           Submit
         </button>
-        {status === 'error' &&
+        {error !== null &&
           <p className='Error'>
-            Good guess but a wrong answer. Try again!
+            {error.message}
           </p>
         }
       </form>
--- src/index.tsx
+++ src/index.tsx
@@ -10,8 +10,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StoryBook />
+    <App />
+    {/* <StoryBook /> */}
   </React.StrictMode>
 );
 
