88ae6955c531c5dd0d0a845115c27be59a376b8f
keunoh <rmsdhcjswo1@gmail.com>
2025-01-27 00:27:44 +0900

    Feat: Queries, Mutations, Query Invalidation

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,52 @@
+import { QueryClient, QueryClientProvider, useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { getTodos, postTodo } from "./tanstack/my-api";
+import { Key, ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
 
-import { useState } from 'react';
-import './Form.css';
-import { resolve } from 'path';
-import { createTodos } from './hook/utils';
-import TodoList from './hook/MyUseMemo';
 
-const todos = createTodos();
+const queryClient = new QueryClient();
 
 export default function App() {
-  const [tab, setTab] = useState('all');
-  const [isDark, setIsDark] = useState(false);
 
   return (
-    <>
-      <button onClick={() => setTab('all')}>
-        All
-      </button>
-      <button onClick={() => setTab('active')}>
-        Active
-      </button>
-      <button onClick={() => setTab('completed')}>
-        Completed
-      </button>
-      <br />
-      <label>
-        <input
-          type="checkbox"
-          checked={isDark}
-          onChange={e => setIsDark(e.target.checked)}
-        />
-        Dark mode
-      </label>
-      <hr />
-      <TodoList
-        todos={todos}
-        tab={tab}
-        theme={isDark ? 'dark' : 'light'}
-      />
-    </>
+    <QueryClientProvider client={queryClient}>
+      <Todos />
+    </QueryClientProvider>
   )
 }
 
+function Todos() {
+  // Access the client
+  const queryClient = useQueryClient();
+
+  // Queries
+  const query = useQuery({ queryKey: ['todos'], queryFn: getTodos });
+
+  // Mutations
+  const mutation = useMutation({
+    mutationFn: postTodo,
+    onSuccess: () => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries({ queryKey: ['todos'] })
+    },
+  })
+
+  return (
+    <div>
+      <ul>
+        {query.data?.map((todo: { id: Key | null | undefined; title: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; }) => <li key={todo.id}>
+          {todo.title}
+        </li>)}
+      </ul>
+      <button
+        onClick={() => {
+          mutation.mutate({
+            id: Date.now(),
+            title: 'Do Laundry',
+          })
+        }}
+      >
+        Add Todo
+      </button>
+    </div>
+  )
+}
